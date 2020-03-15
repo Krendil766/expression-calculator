537587c2d43073cc17663d47d8ddbfb19cd16a20
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-15 23:33:04 +0300

    feat:add expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,9 +4,41 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const str = expr.split('');
+    breckets(str);
+    zero(str);
+
+    let res = expr.replace(/\s/g, '');
+    return new Function('return ' + res)();
+}
+
+function breckets(str) {
+    let counterBreckets = 0;
+
+    for (let key of str) {
+        if (key === "(") {
+            counterBreckets++;
+        }
+        if (key === ")") {
+            counterBreckets--;
+        }
+    }
+
+    if (counterBreckets !== 0) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+}
+
+function zero(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "/") {
+            if (str[i + 2] === "0") {
+                throw new Error("TypeError: Division by zero.");
+            }
+        }
+    }
 }
 
 module.exports = {
     expressionCalculator
-}
+};
